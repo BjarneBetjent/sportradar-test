7ddba15afe3324835f3da8bafeeb104190fa79e0
Olav Magne Smevoll <osmevoll@gmail.com>
2024-08-28 10:52:59 +0200

    Tests for team score and team name

---
+++ src/lib/utils/classes/team.test.js
@@ -0,0 +1,24 @@
+import { expect, test } from 'vitest';
+
+test('Get a teams score', () => {
+	const team = new Team('RBK');
+	expect(team.getScore()).toBe(0);
+});
+
+test('Get a teams name', () => {
+	const team = new Team('RBK');
+	expect(team.getTeamName()).toBe('RBK');
+});
+
+// test for invalid names
+
+test('Update a teams score with a valid value', () => {
+	const team = new Team('RBK');
+	team.updateScore(2);
+	expect(team.getScore()).toBe(2);
+});
+
+test('Update a teams score with an invalid value', () => {
+	const team = new Team('RBK');
+	expect(() => team.updateScore(-1)).toThrowError('Invalid score');
+});
