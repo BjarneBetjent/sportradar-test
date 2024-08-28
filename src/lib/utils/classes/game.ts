f87381ed647edad977d0f4b60bc0bfef7ffd4796
Olav Magne Smevoll <osmevoll@gmail.com>
2024-08-28 13:52:59 +0200

    Added comments

--- src/lib/utils/classes/game.ts
+++ src/lib/utils/classes/game.ts
@@ -1,35 +1,74 @@
 import Team from './team';
 
 export default class Game {
-	homeTeam: Team;
-	awayTeam: Team;
-	timeStarted: number;
-	timeEnded: number | undefined = undefined;
+	private homeTeam: Team;
+	private awayTeam: Team;
+	private timeStarted: number;
+	private timeEnded: number | undefined = undefined;
 
+	/**
+	 * Create a new game
+	 * @param homeTeam Name of home team
+	 * @param awayTeam Name of away team
+	 * @param startTime Start time of the game
+	 */
 	constructor(homeTeam: string, awayTeam: string, startTime?: number) {
 		this.homeTeam = new Team(homeTeam);
 		this.awayTeam = new Team(awayTeam);
 		this.timeStarted = startTime ? startTime : Date.now();
 	}
 
+	/**
+	 * Update the score of the game
+	 * @param homeScore New home score
+	 * @param awayScore New away score
+	 */
 	updateGameScore(homeScore: number, awayScore: number): void {
 		this.homeTeam.updateScore(homeScore);
 		this.awayTeam.updateScore(awayScore);
 	}
 
+	/**
+	 * Finish the game
+	 * @param finishTime What time the game finished. If no argument is provided, the time of function call will be used
+	 */
 	finishGame(finishTime?: number) {
 		this.timeEnded = finishTime ? finishTime : Date.now();
 	}
 
+	/**
+	 * @returns Game start time
+	 */
 	getTimeStarted(): number {
 		return this.timeStarted;
 	}
 
+	/**
+	 * @returns Game end time
+	 */
 	getEndTime(): number | undefined {
 		return this.timeEnded;
 	}
 
+	/**
+	 * Get the total score of the game. Home score + away score
+	 * @returns Total game score
+	 */
 	getTotalScore(): number {
 		return this.homeTeam.getScore() + this.awayTeam.getScore();
+	}
+
+	/**
+	 * @returns Home team
+	 */
+	getHomeTeam(): Team {
+		return this.homeTeam;
+	}
+
+	/**
+	 * @returns Away team
+	 */
+	getAwayTeam(): Team {
+		return this.awayTeam;
 	}
 }
--- src/lib/utils/classes/team.ts
+++ src/lib/utils/classes/team.ts
@@ -2,6 +2,10 @@ export default class Team {
 	private teamName: string;
 	private score: number;
 
+	/**
+	 * Create a new team
+	 * @param teamName Name of the team
+	 */
 	constructor(teamName: string) {
 		if (typeof teamName != 'string' || teamName.length < 2) {
 			throw new Error('Invalid team name');
@@ -10,14 +14,24 @@ export default class Team {
 		this.score = 0;
 	}
 
+	/**
+	 * @returns The team score
+	 */
 	getScore(): number {
 		return this.score;
 	}
 
+	/**
+	 * @returns The team name
+	 */
 	getTeamName(): string {
 		return this.teamName;
 	}
 
+	/**
+	 * Update the score of the score
+	 * @param newScore The new score
+	 */
 	updateScore(newScore: number) {
 		if (typeof newScore != 'number' || newScore < 0) {
 			throw new Error('Invalid score');
