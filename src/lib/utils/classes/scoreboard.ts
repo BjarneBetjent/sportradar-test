e54180a4d33048ac42b476122e1f21f58fc2a1ae
Olav Magne Smevoll <osmevoll@gmail.com>
2024-08-28 13:01:35 +0200

    Added functions to Scoreboard class

--- src/lib/utils/classes/scoreboard.ts
+++ src/lib/utils/classes/scoreboard.ts
@@ -1,3 +1,22 @@
+import Game from './game';
 export default class Scoreboard {
-	//
+	runningGames: Game[];
+
+	constructor() {
+		this.runningGames = [];
+	}
+
+	getGames() {
+		return this.runningGames;
+	}
+
+	addGame(gameToAdd: Game) {
+		this.runningGames.push(gameToAdd);
+	}
+
+	removeGame(gameToRemove: Game) {
+		this.runningGames = this.runningGames.filter(
+			(game) => game.getTimeStarted() != gameToRemove.getTimeStarted()
+		);
+	}
 }
