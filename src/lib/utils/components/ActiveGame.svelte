2eb071f02f7104fbb16750b750588ae0c211d904
Olav Magne Smevoll <osmevoll@gmail.com>
2024-08-28 17:31:06 +0200

    Added finish game button

--- src/lib/utils/components/ActiveGame.svelte
+++ src/lib/utils/components/ActiveGame.svelte
@@ -17,33 +17,41 @@
 			gameID: game.getTimeStarted()
 		});
 	}
+
+	// Called when finish game button is clicked
+	function finishGame() {
+		dispatch('gameFinished', { gameID: game.getTimeStarted() });
+	}
 </script>
 
-<div class="flex w-96 justify-between odd:bg-slate-50">
-	<div class="flex flex-1 p-1 items-center gap-2">
-		<div class="flex-1">
-			<p class="text-slate-600">Home</p>
-			<p class="text-left">{game.getHomeTeam().getTeamName()}</p>
-		</div>
-		<input
-			type="text"
-			class="border w-10 h-10 text-center"
-			bind:value={homeScore}
-			on:input={onScoreChanged}
-		/>
-	</div>
-	<div class="flex flex-1 p-1 items-center gap-2">
-		<div class="flex-1">
+<div class="flex w-96 flex-col odd:bg-slate-50 border">
+	<div class="flex flex-1 justify-between">
+		<div class="flex flex-1 p-1 items-center gap-2">
+			<div class="flex-1">
+				<p class="text-slate-600">Home</p>
+				<p class="text-left">{game.getHomeTeam().getTeamName()}</p>
+			</div>
 			<input
 				type="text"
-				class=" border w-10 h-10 text-center"
-				bind:value={awayScore}
+				class="border w-10 h-10 text-center"
+				bind:value={homeScore}
 				on:input={onScoreChanged}
 			/>
 		</div>
-		<div class="">
-			<p class="text-slate-600 text-right">Away</p>
-			<p class="text-right">{game.getAwayTeam().getTeamName()}</p>
+		<div class="flex flex-1 p-1 items-center gap-2">
+			<div class="flex-1">
+				<input
+					type="text"
+					class=" border w-10 h-10 text-center"
+					bind:value={awayScore}
+					on:input={onScoreChanged}
+				/>
+			</div>
+			<div class="">
+				<p class="text-slate-600 text-right">Away</p>
+				<p class="text-right">{game.getAwayTeam().getTeamName()}</p>
+			</div>
 		</div>
 	</div>
+	<button on:click={finishGame} class="bg-red-50 border-t hover:bg-red-200">Finish Game</button>
 </div>
