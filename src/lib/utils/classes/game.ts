6336913ed2492e057dbad8298f5456ee7746b240
Olav Magne Smevoll <osmevoll@gmail.com>
2024-08-28 11:42:44 +0200

    Created the game class

--- src/lib/utils/classes/game.ts
+++ src/lib/utils/classes/game.ts
@@ -1,3 +1,35 @@
 import Team from './team';
 
-export default class Game {}
+export default class Game {
+	homeTeam: Team;
+	awayTeam: Team;
+	timeStarted: number;
+	timeEnded: number | undefined = undefined;
+
+	constructor(homeTeam: string, awayTeam: string, startTime?: number) {
+		this.homeTeam = new Team(homeTeam);
+		this.awayTeam = new Team(awayTeam);
+		this.timeStarted = startTime ? startTime : Date.now();
+	}
+
+	updateGameScore(homeScore: number, awayScore: number): void {
+		this.homeTeam.updateScore(homeScore);
+		this.awayTeam.updateScore(awayScore);
+	}
+
+	finishGame(finishTime?: number) {
+		this.timeEnded = finishTime ? finishTime : Date.now();
+	}
+
+	getTimeStarted(): number {
+		return this.timeStarted;
+	}
+
+	getEndTime(): number | undefined {
+		return this.timeEnded;
+	}
+
+	getTotalScore(): number {
+		return this.homeTeam.getScore() + this.awayTeam.getScore();
+	}
+}
